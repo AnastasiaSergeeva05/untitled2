20c8e0ae277d58a6bd57f1bce74077e664b8193c
AnastasiaSergeeva05 <112942778+AnastasiaSergeeva05@users.noreply.github.com>
2023-01-21 13:02:24 +0300

    курсовая

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String[] avtor = new String[]{"Лев Толстой", "Война и Мир"};
+        String[] nameBook = new String[]{"Александр Пушкин", "Капитанская дочка"};
+
+        for (int i = 0; i < 2; i++) {
+
+            System.out.println(avtor[i]);
+
+            System.out.println(nameBook[i]);
+
+        }
+
+    }
+}
